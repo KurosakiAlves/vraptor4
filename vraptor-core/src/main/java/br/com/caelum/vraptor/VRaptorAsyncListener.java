5c82fd25dffd71c5e48a01548654803b925d4d8c
Kurosaki Alves <kurosaki_alves@hotmail.com>
2015-06-13 19:18:21 -0300

    VRaptor - added the possibility to make async requests.

--- vraptor-core/src/main/java/br/com/caelum/vraptor/VRaptorAsyncListener.java
+++ vraptor-core/src/main/java/br/com/caelum/vraptor/VRaptorAsyncListener.java
@@ -1,8 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/***
+ * Copyright (c) 2009 Caelum - www.caelum.com.br/opensource
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ * 
+ * 	http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License. 
  */
+
 package br.com.caelum.vraptor;
 
 import java.io.IOException;
@@ -12,8 +24,8 @@ import javax.servlet.ServletRequest;
 import org.slf4j.Logger;
 
 /**
- *
- * @author A-IKASORUK
+ * Used to listen to the asynchronous event.
+ * @author Guilherme Alves
  */
 public class VRaptorAsyncListener implements AsyncListener
 {
